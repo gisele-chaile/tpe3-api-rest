a6c1b7f47968c818ef313eaed202035c217c1fb5
gisele-chaile <giselechaile@gmail.com>
2024-11-10 18:30:00 -0300

    Update books.api.controller.php

--- app/controller/books.api.controller.php
+++ app/controller/books.api.controller.php
@@ -10,39 +10,32 @@ class BooksApiController {
         $this->model = new BooksModel();
         $this->view = new JSONView();
     }
-    //obtengo todos los libros
+    // obtengo todos los libros
     public function getAll($req, $res) {
-        if(!$res->user) {
-            return $this->view->response("No autorizado", 401);
-        }
-
         $orderBy = false;
         if(isset($req->query->orderBy))
             $orderBy = $req->query->orderBy;
 
-        $sort = 'asc';
-        if (isset($req->query->sort)) 
-            $sort = $req->query->sort;
-
         $genero = null;
-        if(isset($req->query->ID_genero))
+        if(isset($req->query->ID_genero)) {
             $genero = $req->query->ID_genero;
+        }
 
-        $books = $this->model->getBooks($orderBy, $sort, $genero);
-        
+        $books = $this->model->getBooks($genero, $orderBy);
+
         return $this->view->response($books);
     }
 
-    //obtengo libros por id
+    // obtengo libro por id
     public function get($req, $res) {
-        
-        $id = $req->params->ID_libro;
+        // obtengo el id del libro desde la ruta
+        $id = $req->params->id;
 
         // obtengo el libro de la DB
         $book = $this->model->getBook($id);
 
         if(!$book) {
-            return $this->view->response("La tarea con el id=$id no existe", 404);
+            return $this->view->response(" El libro con el id=$id no existe", 404);
         }
 
         // mando el libro a la vista
@@ -53,19 +46,19 @@ class BooksApiController {
     public function create($req, $res) {
 
         // valido los datos
-        if (empty($req->body->Titulo) || empty($req->body->Autor) || empty($req->body->Reseña) || empty($req->body->Año)) {
+        if (empty($req->body->Titulo) || empty($req->body->Autor)|| empty($req->body->Reseña)|| empty($req->body->Año)) {
             return $this->view->response('Faltan completar datos', 400);
         }
 
         // obtengo los datos
         $titulo = $req->body->Titulo;       
         $autor = $req->body->Autor;       
-        $reseña = $req->body->Reseña; 
+        $reseña = $req->body->Reseña;
         $año = $req->body->Año;
-        $genero = $req->body->ID_genero;    
+        $genero = $req->body->ID_genero;      
 
         // inserto los datos
-        $id = $this->model->insertBook($titulo, $autor, $reseña, $año, $genero);
+        $id = $this->model->insertBook($titulo, $autor, $reseña,$año,$genero);
 
         if (!$id) {
             return $this->view->response("Error al insertar libro", 500);
@@ -85,25 +78,28 @@ class BooksApiController {
             return $this->view->response("El libro con el id=$id no existe", 404);
         }
 
-       // valido los datos
-        if (empty($req->body->Titulo) || empty($req->body->Autor) || empty($req->body->Reseña) || empty($req->body->Año)) {
+         // valido los datos
+         if (empty($req->body->Titulo) || empty($req->body->Autor)|| empty($req->body->Reseña)|| empty($req->body->Año)) {
             return $this->view->response('Faltan completar datos', 400);
         }
 
         // obtengo los datos
         $titulo = $req->body->Titulo;       
         $autor = $req->body->Autor;       
-        $reseña = $req->body->Reseña; 
+        $reseña = $req->body->Reseña;
         $año = $req->body->Año;
-        $genero = $req->body->ID_genero; 
+        $genero = $req->body->ID_genero;
 
-      
-        $this->model->updateBook($id, $titulo, $autor, $reseña, $año, $genero);
+        // actualiza el libro
+        $this->model->updateBook($id,$titulo, $autor, $reseña,$año,$genero);
 
-        // obtengo el libro modificado y lo devuelvo en la respuesta
+        // obtengo el libro modificad y lo devuelvo en la respuesta
         $book = $this->model->getBook($id);
         $this->view->response($book, 200);
     }
 
+
 }
+
+
 
